3f63b5b055096dcc169bb19d11f294a18b0555c9
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2023-07-01 16:30:56 -0300

    adicionando interações com js

--- script/script.js
+++ script/script.js
@@ -16,6 +16,21 @@ window.addEventListener('load', function() {
     var svgParent = document.getElementById('SVG_Object')
     let svgDoc = svgParent.contentDocument
     let svgElement = svgDoc.getElementById('elementoSVG')
-    console.log(svgElement)
 });
 
+let todosPortifolios = document.querySelectorAll('.portifolio')
+
+todosPortifolios.forEach((n)=>{
+    let h2 = n.querySelector('.titulo-projeto')
+
+    n.addEventListener('mouseenter',()=>{
+        n.style.background = "rgba(121,30,88, 0.1)"
+        n.style.cursor = "pointer"
+        h2.style.color = 'rgb(94 234 212)'
+    })
+
+    n.addEventListener('mouseleave',()=>{
+        n.style.background = '#0D1024'
+        h2.style.color = '#fff'
+    })
+})
