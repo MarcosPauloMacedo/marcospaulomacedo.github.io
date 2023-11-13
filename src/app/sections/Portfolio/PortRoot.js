71a69583a43ffc195f196a321506bb9d29ab939a
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2023-11-13 16:50:39 -0300

    removendo css desnecessário

--- src/app/sections/Portfolio/PortRoot.js
+++ src/app/sections/Portfolio/PortRoot.js
@@ -1,8 +1,6 @@
-import styles from './port.module.css'
-
 export function PortRoot({children}){
     return(
-        <section className={`mt-4 col-md-6 ${styles.portifolio}`} id="Portifolio">
+        <section className={"mt-4 col-md-6"} id="Portifolio">
             {children}
         </section>
     )
