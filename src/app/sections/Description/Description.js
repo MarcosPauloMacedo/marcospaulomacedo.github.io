1f99a12f708949601ab2d25f99bd0823c9c93898
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-04-14 01:59:17 +0000

    alterando texto da descrição

--- src/app/sections/Description/Description.js
+++ src/app/sections/Description/Description.js
@@ -3,7 +3,7 @@ import styles from './description.module.css';
 export function Description() {
     return(
         <div className="col-md-6 p-5">
-            <p className={styles.color}>Desenvolvido com <span className={`${styles.colorSpan}`}>Next.js</span> para uma experiência dinâmica e <span className={`${styles.colorSpan}`}>Bootstrap</span> para uma estrutura sólida e responsiva. A implantação é feita com a eficiência do <span className={`${styles.colorSpan}`}>GitPages.</span> </p>
+            <p className={styles.color}>Desenvolvido com <span className={`${styles.colorSpan}`}>Next.js</span> para uma experiência dinâmica e <span className={`${styles.colorSpan}`}>Bootstrap</span> para uma estrutura sólida e responsiva. A implantação é feita pela ferramenta DX-Plataform da <span className={`${styles.colorSpan}`}>Vercel.</span> </p>
         </div>
     )
 }
