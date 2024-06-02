19ea8de4bc59ec260f0424f68eca84df41b2d66a
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-06-02 14:54:06 -0300

    alterando lógica do alterTheme

--- src/app/server/alterTheme.js
+++ src/app/server/alterTheme.js
@@ -4,7 +4,11 @@ import { cookies } from "next/headers"
 import { theme, dark, light } from "../cookies/themes"
 
 export default async function alterTheme(){
-    const themeValue = cookies().get(theme).value
-    const newTheme = themeValue === dark ? light : dark
-    cookies().set(theme, newTheme)
+    if(cookies().has(theme)){
+        const themeValue = cookies().get(theme).value
+        const newTheme = themeValue === dark ? light : dark
+        cookies().set(theme, newTheme)
+    } else {
+        cookies().set(theme, light)
+    }
 }
